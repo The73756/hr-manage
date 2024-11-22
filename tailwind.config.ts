838c6cdfbe4daa56f6d5b78fcdbc8e141f44b921
katya-sy <katya_sy_28@mail.ru>
2024-11-22 13:19:43 +0900

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,46 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    screens: {
+      xs: "480px",
+      sm: "576px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1440px",
+      "2xl": "1536px",
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        xs: "100%",
+        sm: "100%",
+        md: "100%",
+        lg: "100%",
+        xl: "1332px",
+        "2xl": "1332px",
+      },
+    },
+    extend: {
+      colors: {
+        blue: {
+          DEFAULT: "#0000ff",
+          light: "#4169e1",
+        },
+        red: "#f25252",
+        green: "#5ceb51",
+      },
+      fontFamily: {
+        manrope: "Manrope",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
