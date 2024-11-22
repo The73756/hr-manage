b8ef1ca53f8b1e13175fa0a0c774a55cdd82140e
katya-sy <katya_sy_28@mail.ru>
2024-11-22 17:17:33 +0900

    add exit modal

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,16 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { DialogClose } from "@radix-ui/react-dialog";
 
 export const Header = () => {
   const user = true;
@@ -11,7 +21,32 @@ export const Header = () => {
         <Link href="/">
           <Image src="/logo.svg" alt="logo" width={154} height={70} />
         </Link>
-        {user && <Button intent="secondary">Выйти</Button>}
+        {user && (
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button intent="secondary">Выйти</Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Выйти из сервиса?</DialogTitle>
+                <DialogDescription>
+                  Время работы будет сохранено
+                </DialogDescription>
+              </DialogHeader>
+              <DialogFooter>
+                <Button className="bg-red w-full text-white">Выйти</Button>
+                <DialogClose asChild>
+                  <Button
+                    intent="secondary"
+                    className="border-blue w-full text-blue"
+                  >
+                    Отменить
+                  </Button>
+                </DialogClose>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+        )}
       </div>
     </div>
   );
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -2,11 +2,11 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { twMerge } from "tailwind-merge";
 
 const buttonVariants = cva(
-  "rounded-lg font-bold whitespace-nowrap transition-colors duration-300",
+  "rounded-lg font-bold text-xl whitespace-nowrap transition-colors duration-300",
   {
     variants: {
       intent: {
-        primary: "text-xl px-8 py-2 bg-blue text-white",
+        primary: "px-8 py-2 bg-blue text-white",
         secondary: "px-8 py-2 text-red border border-red",
         icon: "flex justify-center text-grey hover:text-beige items-center bg-beige rounded-full w-12 h-12",
       },
@@ -23,13 +23,14 @@ interface ButtonVariants
 
 export const Button = ({
   className,
+  onClick,
   children,
   ...variants
 }: ButtonVariants & React.HTMLProps<HTMLButtonElement>) => {
   const classes = twMerge(buttonVariants(variants), className);
 
   return (
-    <button className={classes} {...variants}>
+    <button className={classes} onClick={onClick} {...variants}>
       {children}
     </button>
   );
