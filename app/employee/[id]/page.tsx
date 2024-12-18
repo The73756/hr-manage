2d59f4ad83efdc187c517858e04ccd51e17fd4d0
katya-sy <katya_sy_28@mail.ru>
2024-12-19 02:46:25 +0900

    fix

--- app/employee/[id]/page.tsx
+++ app/employee/[id]/page.tsx
@@ -4,7 +4,7 @@ import {User} from "@/types/user";
 import {Schedule} from "@/types/schedule";
 
 export default async function OneEmployeePage() {
-  const {employees, schedules, workDays, salaries} = (await getEmployees()) as {employees: User[], schedules: Schedule[], workDays: any, schedules: any};
+  const {employees, schedules, workDays, salaries} = (await getEmployees()) as {employees: User[], schedules: Schedule[], workDays: any, salaries: any};
   return (
     <EmployeePage data={{employees, schedules, workDays, salaries}} />
   );
--- components/filters.tsx
+++ components/filters.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import {
   Select,
   SelectContent,
@@ -7,24 +7,27 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { Input } from "@/components/ui/input";
-import {useEmployeeStore} from "@/store/employees-store";
-import {useState} from "react";
+import { useEmployeeStore } from "@/store/employees-store";
+import { useState } from "react";
 
 export const Filters = () => {
-  const currentDate = useEmployeeStore(state => state.currentDate)
-  const setCurrentDate = useEmployeeStore(state => state.setCurrentDate)
-  const searchQuery = useEmployeeStore(state => state.searchQuery)
-  const setSearchQuery = useEmployeeStore(state => state.setSearchQuery)
+  const currentDate = useEmployeeStore((state) => state.currentDate);
+  const setCurrentDate = useEmployeeStore((state) => state.setCurrentDate);
+  const searchQuery = useEmployeeStore((state) => state.searchQuery);
+  const setSearchQuery = useEmployeeStore((state) => state.setSearchQuery);
 
-  const [date, setDate] = useState(currentDate)
+  const [date, setDate] = useState(currentDate);
 
   return (
     <div className="flex max-[800px]:flex-wrap flex-grow flex-shrink-0 gap-5 max-w-full">
       <div className="flex max-sm:flex-wrap gap-2 sm:gap-5">
-        <Select value={date} onValueChange={(val) => {
-          setCurrentDate(val)
-          setDate(val)
-        }}>
+        <Select
+          value={date}
+          onValueChange={(val) => {
+            setCurrentDate(val);
+            setDate(val);
+          }}
+        >
           <SelectTrigger className="w-full xs:w-[120px] min-w-[120px]">
             <SelectValue placeholder="Дата" />
           </SelectTrigger>
@@ -49,7 +52,7 @@ export const Filters = () => {
               <SelectItem value="3">Фильтр 3</SelectItem>
             </SelectContent>
           </Select> */}
-          <Select>
+          {/* <Select>
             <SelectTrigger className="max-xs:w-full min-w-[150px] xs:max-w-[200px]">
               <SelectValue placeholder="Сортировать" />
             </SelectTrigger>
@@ -58,12 +61,17 @@ export const Filters = () => {
               <SelectItem value="2">По часам</SelectItem>
               <SelectItem value="3">По зарплате</SelectItem>
             </SelectContent>
-          </Select>
+          </Select> */}
         </div>
       </div>
-      <Input className="min-w-[270px]" value={searchQuery} onChange={(e) => {
-        setSearchQuery(e.target.value)
-      }} placeholder="Найти работника..." />
+      <Input
+        className="min-w-[270px]"
+        value={searchQuery}
+        onChange={(e) => {
+          setSearchQuery(e.target.value);
+        }}
+        placeholder="Найти работника..."
+      />
     </div>
   );
 };
