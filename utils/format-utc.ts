d0847544d874b9699224201ddd9c51babf744d6f
The73756 <the73756the@gmail.com>
2024-12-26 10:37:12 +0900

    fix time

--- utils/format-utc.ts
+++ utils/format-utc.ts
@@ -1,5 +1,6 @@
 export const formatUTC = (time: string | undefined) => {
   if (!time) return
+  console.log(time)
   return Intl.DateTimeFormat(undefined, {
     timeZone: 'Asia/Tokyo',
     hour: '2-digit',
@@ -8,7 +9,7 @@ export const formatUTC = (time: string | undefined) => {
     new Date().getFullYear(),
     new Date().getMonth(),
     new Date().getDate(),
-    Number(time.split(":")[0]),
+    Number(time.split(":")[0]) + 9,
     Number(time.split(":")[1])
   ))
 }
